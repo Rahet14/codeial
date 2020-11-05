e8e4c0927201924ae36e509e1fb319bcb166787c
Rahet Tuteja <rahettuteja@gmail.com>
2020-11-05 18:56:36 +0530

    setting up mongo store to store the session cookie

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const db = require("./config/mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-stratergy");
+const MongoStore = require("connect-mongo")(session);
 
 app.use(express.urlencoded());
 
@@ -34,6 +35,15 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 100,
     },
+    store: new MongoStore(
+      {
+        mongooseConnection: db,
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect mongodb set up okay");
+      }
+    ),
   })
 );
 
